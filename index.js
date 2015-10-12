fe5e8cfb8103614762c224674ce83f65b164504a
Akash Nimare <svnitakash@gmail.com>
2015-10-12 15:42:44 +0530

    bugs fixed

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 var screenshot = require('url-to-screenshot');
 var fs = require('fs');
-var path = require('path');
 var argv = require('yargs')
     .usage('Usage: $0 -u [url] -w [width] -h [height] -n [Screenshotname]')
     .demand(['u','w','h','n'])
@@ -10,6 +9,7 @@ var argv = require('yargs')
 screenshot(argv.u)
   .width(argv.w)
   .height(argv.h)
+  .clip()
   .capture(function(err, img) {
     if (err) throw err;
     fs.writeFile(argv.n + ".png" , img)
